887495dee8e01f95dfd9f87b39a1ff99b9f85d5f
Ilse <140922576+suns1ck@users.noreply.github.com>
2026-01-12 15:20:17 +1300

    Add partial first function
    Wrote part of the first function. Also corrected the yet-to-be-used variable values from "null" to "None", making it less annoying to look at.

--- docker-compose-deployment-autoimportdata.py
+++ docker-compose-deployment-autoimportdata.py
@@ -1,9 +1,29 @@
+import os
+import subprocess
+
 compose_dict = {}
 
-compose_scrape = null
-compose_validation = null
-container_start = null
-deployment_check = null
+def compose_collect(compose_dict):
+
+    # This function requests the compose file location from the user, collects the contained data, and places it into compose_dict.
+
+    additional_deployment = None
+
+    while additional_deployment != 'N':
+        compose_location = input("Please enter the full path to your docker compose file (/dir/compose.yml): ").strip()
+
+        if os.path.exists(compose_location):
+            try:
+                with open(compose_location, 'r') as compose:
+                    None
+            except:
+                None
+        if not os.path.exists(compose_location):
+            print("The specified file does not exist. Check everything is in order and try again.")
+
+compose_validation = None
+container_start = None
+deployment_check = None
 
 def main():
     compose_collect()
